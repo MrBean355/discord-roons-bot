6f77e7703f0942f7143299cb27b9b03741073884
Mike Johnston <mrbean355@gmail.com>
2019-08-14 14:34:32 +0200

    Update README

--- src/main/kotlin/com/github/mrbean355/roons/Config.kt
+++ src/main/kotlin/com/github/mrbean355/roons/Config.kt
@@ -2,7 +2,7 @@ package com.github.mrbean355.roons
 
 const val SOUND_FILE_NAME = "roons.mp3"
 const val COMMAND_PREFIX = '!'
-const val HELP_URL = "https://github.com/MrBean355/discord-roons-bot"
+const val HELP_URL = "https://github.com/MrBean355/admiralbulldog-sounds/wiki/Discord-Bot"
 const val VOLUME_MIN = 0
 const val VOLUME_MAX = 100
 const val VOLUME_DEFAULT = 35
