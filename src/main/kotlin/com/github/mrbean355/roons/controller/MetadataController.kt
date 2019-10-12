52dc46a963f4fde6dc4b0116a15313745dd88122
Mike Johnston <mrbean355@gmail.com>
2019-10-12 09:45:08 +0200

    Implement service to get for later version

---
+++ src/main/kotlin/com/github/mrbean355/roons/controller/MetadataController.kt
@@ -0,0 +1,20 @@
+package com.github.mrbean355.roons.controller
+
+import com.vdurmont.semver4j.Semver
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod.GET
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+/** Current stable version of the client app. */
+private val LATEST_VERSION = Semver("1.5.1")
+
+@RestController
+@RequestMapping("/metadata")
+class MetadataController {
+
+    @RequestMapping("laterVersion", method = [GET])
+    fun hasLaterVersion(@RequestParam("version") version: String): Boolean {
+        return Semver(version) < LATEST_VERSION
+    }
+}
