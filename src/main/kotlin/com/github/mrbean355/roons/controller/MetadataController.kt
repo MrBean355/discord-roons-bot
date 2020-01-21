dbd0e0da804a239495b5cd63258e910d648b930e
Mike Johnston <mrbean355@gmail.com>
2020-01-21 19:51:08 +0200

    Simplify hasLaterVersion service
    As of app version 1.8.0, we no longer call this service to check for a new app release.
    Instead, we use the GitHub API to determine what the latest release is.
    Therefore, only old apps will call this service. There will always be a new version for them.

--- src/main/kotlin/com/github/mrbean355/roons/controller/MetadataController.kt
+++ src/main/kotlin/com/github/mrbean355/roons/controller/MetadataController.kt
@@ -1,9 +1,6 @@
 package com.github.mrbean355.roons.controller
 
-import com.github.mrbean355.roons.repository.MetadataRepository
-import com.vdurmont.semver4j.Semver
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.GET
@@ -12,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/metadata")
-class MetadataController @Autowired constructor(private val metadataRepository: MetadataRepository) {
+class MetadataController @Autowired constructor() {
 
     @RequestMapping("laterVersion", method = [GET])
     fun hasLaterVersion(@RequestParam("version") version: String): ResponseEntity<Boolean> {
-        val metadata = metadataRepository.findByKey("latest_app_version")
-                ?: return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
-
-        return ResponseEntity.ok(Semver(version) < Semver(metadata.value))
+        // As of app version 1.8.0, we no longer call this service to check for a new app release.
+        // Instead, we use the GitHub API to determine what the latest release is.
+        // Therefore, only old apps will call this service. There will always be a new version for them.
+        return ResponseEntity.ok(true)
     }
 }
