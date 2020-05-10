267f07f5b43b7aac7309a84f734d8349e60c59e2
Mike Johnston <mrbean355@gmail.com>
2020-05-10 14:14:40 +0200

    Add /users Telegram command
    Shows how many users have been active in the last 5 minutes.

--- src/main/kotlin/com/github/mrbean355/roons/repository/AppUserRepository.kt
+++ src/main/kotlin/com/github/mrbean355/roons/repository/AppUserRepository.kt
@@ -2,9 +2,10 @@ package com.github.mrbean355.roons.repository
 
 import com.github.mrbean355.roons.AppUser
 import org.springframework.data.repository.CrudRepository
+import java.util.Date
 
 interface AppUserRepository : CrudRepository<AppUser, Int> {
     fun countByGeneratedId(generatedId: String): Int
     fun findByGeneratedId(generatedId: String): AppUser?
+    fun findByLastSeenAfter(date: Date): List<AppUser>
 }
-
--- src/main/kotlin/com/github/mrbean355/roons/telegram/RoonsTelegramBot.kt
+++ src/main/kotlin/com/github/mrbean355/roons/telegram/RoonsTelegramBot.kt
@@ -1,15 +1,43 @@
 package com.github.mrbean355.roons.telegram
 
+import com.github.mrbean355.roons.repository.AppUserRepository
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import java.util.Calendar
+import java.util.Date
 
 @Component
-class RoonsTelegramBot : TelegramLongPollingBot() {
+class RoonsTelegramBot(private val appUserRepository: AppUserRepository) : TelegramLongPollingBot() {
+    private val chatId = System.getenv("TELEGRAM_CHAT")?.toLong()
 
-    override fun getBotUsername(): String = "RoonsAlertBot"
+    override fun getBotUsername(): String = System.getenv("TELEGRAM_USERNAME")
 
     override fun getBotToken(): String = System.getenv("TELEGRAM_TOKEN")
 
-    override fun onUpdateReceived(update: Update?) {}
+    override fun onUpdateReceived(update: Update) {
+        if (!update.hasMessage() || !update.message.hasText()) {
+            return
+        }
+        if (update.message.text == "/users") {
+            usersCommand()
+        }
+    }
+
+    private fun usersCommand() {
+        val activeUsers = appUserRepository.findByLastSeenAfter(fiveMinutesAgo()).size
+        val message = buildString {
+            append("📊 <b>Active Users:</b>\n")
+            append("There have been <b>$activeUsers</b> active users in the last 5 minutes.")
+        }
+        execute(SendMessage(chatId, message).enableHtml(true))
+    }
+
+    private fun fiveMinutesAgo(): Date {
+        return Calendar.getInstance().run {
+            add(Calendar.MINUTE, -5)
+            time
+        }
+    }
 }
