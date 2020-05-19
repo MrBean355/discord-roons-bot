8706042af1bc577d587802473fb22f643d86946d
Mike Johnston <mrbean355@gmail.com>
2020-05-19 19:30:58 +0200

    Implement heartbeat service

--- src/main/kotlin/com/github/mrbean355/roons/controller/UserController.kt
+++ src/main/kotlin/com/github/mrbean355/roons/controller/UserController.kt
@@ -4,11 +4,13 @@ import com.github.mrbean355.roons.AppUser
 import com.github.mrbean355.roons.CreateIdResponse
 import com.github.mrbean355.roons.component.Statistics
 import com.github.mrbean355.roons.repository.AppUserRepository
+import com.github.mrbean355.roons.repository.updateLastSeen
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.POST
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.Date
 import java.util.UUID
@@ -32,5 +34,10 @@ class UserController @Autowired constructor(
         appUserRepository.save(AppUser(0, generated, Date()))
         statistics.increment(Statistics.Type.NEW_USERS)
         return ResponseEntity.ok(CreateIdResponse(generated))
+    }
+
+    @RequestMapping("heartbeat")
+    fun heartbeat(@RequestParam("userId") userId: String) {
+        appUserRepository.updateLastSeen(userId)
     }
 }
