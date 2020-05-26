ba08c2b037a062b291793e0034d531351ffaf567
Mike Johnston <mrbean355@gmail.com>
2020-05-26 22:43:24 +0200

    Improve sound bite synchronising
    When synchronising, re-download all sound bites:
    - This ensures that ones which changed (but kept the same name) will be updated.
    
    Implement a "mirror" sounds directory:
    - When synchronising, sounds will be downloaded into the "mirror" directory.
    - Once sync is complete, the active directory and "mirror" directory will be flipped.
    - This ensures there's no downtime when synchronising.

--- src/main/kotlin/com/github/mrbean355/roons/discord/SoundStore.kt
+++ src/main/kotlin/com/github/mrbean355/roons/discord/SoundStore.kt
@@ -3,25 +3,21 @@ package com.github.mrbean355.roons.discord
 import com.github.mrbean355.roons.component.PlaySounds
 import com.github.mrbean355.roons.component.Statistics
 import com.github.mrbean355.roons.telegram.TelegramNotifier
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.io.File
+import java.io.FileOutputStream
 import java.math.BigInteger
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.security.MessageDigest
-import java.util.concurrent.ConcurrentLinkedQueue
-import java.util.concurrent.CopyOnWriteArrayList
-
-/** Folder to store downloaded sounds in. */
-private const val SOUNDS_PATH = "sounds"
+import javax.annotation.PostConstruct
 
 /** Folder within resources where special sounds live. */
 private const val SPECIAL_SOUNDS_PATH = "special_sounds"
@@ -36,78 +32,56 @@ class SoundStore @Autowired constructor(
         private val logger: Logger,
         private val statistics: Statistics
 ) {
-    private val fileChecksums = mutableMapOf<String, String>()
-    private val mutex = Mutex()
-    private var firstSync = true
+    private val coroutineScope = CoroutineScope(IO + SupervisorJob())
+    private var soundsDirectory = SoundsDirectory.PRIMARY
+    private var fileChecksums: Map<String, String> = emptyMap()
 
-    /** @return names of all downloaded sounds. */
+    @PostConstruct
+    fun onPostConstruct() {
+        runBlocking {
+            coroutineScope.launch {
+                fileChecksums = downloadAllSoundBites(soundsDirectory)
+            }.join() // wait for sound bites to download.
+        }
+    }
+
+    /** @return map of sound bite name to its file's checksum. */
     fun listAll(): Map<String, String> {
-        waitForSynchronising()
         return fileChecksums
     }
 
     /** @return [File] for the specified [soundFileName] if it exists, `null` otherwise. */
     fun getFile(soundFileName: String): File? {
-        waitForSynchronising()
-        return getFileInternal(soundFileName)
+        return soundsDirectory.getSound(soundFileName.trim())
     }
 
     /** @return `true` if the sound file exists, `false` otherwise. */
     fun soundExists(soundFileName: String): Boolean {
-        waitForSynchronising()
-        return soundExistsInternal(soundFileName)
+        return getFile(soundFileName) != null
     }
 
     /**
-     * Sync our local sounds with the PlaySounds page.
-     * Downloads sounds which don't exist locally.
-     * Deletes local sounds which don't exist remotely.
-     * Scheduled for once per hour.
+     * Synchronise our collection of sound bites with the PlaySounds page.
+     * Re-downloads all sounds to make sure that sounds with the same name but different content are also downloaded.
+     * Downloads into the non-active [SoundsDirectory] and then flips the active & non-active directories.
      */
-    @Scheduled(fixedRate = 3_600_000)
-    fun synchroniseSounds() = GlobalScope.launch {
-        mutex.withLock {
-            val localFiles = ConcurrentLinkedQueue(getLocalFiles())
-            val remoteFiles = playSounds.listRemoteFiles()
-            val newFiles = CopyOnWriteArrayList<String>()
-
-            coroutineScope {
-                remoteFiles.forEach { remoteFile ->
-                    launch {
-                        val existsLocally = localFiles.remove(remoteFile.localFileName)
-                        if (!existsLocally) {
-                            try {
-                                playSounds.downloadFile(remoteFile, SOUNDS_PATH)
-                                newFiles += remoteFile.fileName
-                                logger.info("Downloaded: ${remoteFile.localFileName}")
-                            } catch (t: Throwable) {
-                                logger.error("Failed to download $remoteFile", t)
-                            }
-                        }
-                    }
-                }
-            }
-            localFiles.forEach {
-                File("$SOUNDS_PATH/$it").delete()
-                logger.info("Deleted old sound: $it")
-            }
-            copySpecialSounds()
-
-            fileChecksums.clear()
-            fileChecksums += File(SOUNDS_PATH).listFiles()?.toList().orEmpty()
-                    .associateWith { it.checksum() }
-                    .mapKeys { it.key.name }
-
-            logger.info("Done synchronising")
-            if (firstSync) {
-                firstSync = false
-            } else {
-                sendTelegramNotification(newFiles, localFiles)
-            }
+    @Scheduled(initialDelayString = "PT1H", fixedRateString = "PT1H")
+    fun synchroniseSoundBites() {
+        coroutineScope.launch {
+            val old = fileChecksums
+            val nextDirectory = soundsDirectory.other()
+            val new = downloadAllSoundBites(nextDirectory)
+            fileChecksums = new
+            soundsDirectory = nextDirectory
+            sendTelegramNotification(
+                    newFiles = new.keys - old.keys,
+                    changedFiles = new.filter { it.key in old.keys }.filter { it.value != old.getValue(it.key) }.keys,
+                    oldFiles = old.keys - new.keys
+            )
         }
     }
 
-    @Scheduled(fixedRate = 86_400_000)
+    @Scheduled(fixedRateString = "P1D")
     fun sendStatisticsNotification() {
         if (statistics.isEmpty()) {
             return
@@ -120,19 +94,51 @@ class SoundStore @Autowired constructor(
             """.trimIndent())
     }
 
-    private fun sendTelegramNotification(newFiles: Collection<String>, oldFiles: Collection<String>) {
+    private suspend fun downloadAllSoundBites(soundsDirectory: SoundsDirectory): Map<String, String> {
+        val destination = File(soundsDirectory.dirName)
+        if (destination.exists()) {
+            destination.deleteRecursively()
+        }
+        destination.mkdirs()
+
+        coroutineScope {
+            playSounds.listRemoteFiles().forEach { remoteSoundFile ->
+                launch {
+                    try {
+                        playSounds.downloadFile(remoteSoundFile, soundsDirectory.dirName)
+                    } catch (t: Throwable) {
+                        logger.error("Failed to download $remoteSoundFile", t)
+                    }
+                }
+            }
+        }
+
+        copySpecialSounds(soundsDirectory)
+
+        return destination.listFiles()?.toList().orEmpty()
+                .associateWith { it.checksum() }
+                .mapKeys { it.key.name }
+    }
+
+    private fun sendTelegramNotification(newFiles: Collection<String>, changedFiles: Collection<String>, oldFiles: Collection<String>) {
         val message = buildString {
             if (newFiles.isNotEmpty()) {
-                append("New sounds: $newFiles")
+                append("New: ${newFiles.joinToString()}")
+            }
+            if (changedFiles.isNotEmpty()) {
+                if (isNotEmpty()) {
+                    append("\n")
+                }
+                append("Changed: ${changedFiles.joinToString()}")
             }
             if (oldFiles.isNotEmpty()) {
                 if (isNotEmpty()) {
                     append("\n")
                 }
-                append("Old sounds: $oldFiles")
+                append("Old: ${oldFiles.joinToString()}")
             }
             if (isNotEmpty()) {
-                insert(0, "🔊 <b>Synchronised sounds</b>:\n")
+                insert(0, "🔊 <b>Updated sound bites</b>:\n")
             }
         }
         if (message.isNotEmpty()) {
@@ -140,54 +146,17 @@ class SoundStore @Autowired constructor(
         }
     }
 
-    /**
-     * Wait for the [synchroniseSounds] method to finish downloading all sounds (max 10 seconds).
-     * We don't want to allow the app to check what sounds exist if we're still downloading them.
-     * Otherwise an incomplete list of sounds will be returned.
-     */
-    private fun waitForSynchronising() {
-        var attempts = 0
-        while (mutex.isLocked) {
-            Thread.sleep(100)
-            ++attempts
-            if (attempts >= 100) {
-                throw IllegalStateException("Waited too long sounds to synchronise")
-            }
-        }
-    }
-
-    private fun getFileInternal(soundFileName: String): File? {
-        val file = File("$SOUNDS_PATH/${soundFileName.trim()}")
-        if (file.exists()) {
-            return file
-        }
-        return null
-    }
-
-    private fun soundExistsInternal(soundFileName: String): Boolean {
-        return getFileInternal(soundFileName) != null
-    }
-
-    /** @return a list of local sounds (excluding special ones). */
-    private fun getLocalFiles(): List<String> {
-        val root = File(SOUNDS_PATH)
-        if (!root.exists()) {
-            root.mkdirs()
-            return emptyList()
-        }
-        return root.list()?.toList().orEmpty().filter {
-            it !in SPECIAL_SOUNDS
-        }
-    }
-
     /** Copy special sounds from resources to the destination folder. */
-    private fun copySpecialSounds() {
-        SPECIAL_SOUNDS.forEach {
-            if (!File("$SOUNDS_PATH/$it").exists()) {
-                val stream = SoundStore::class.java.classLoader.getResourceAsStream("$SPECIAL_SOUNDS_PATH/$it")
-                if (stream != null) {
-                    Files.copy(stream, Paths.get("$SOUNDS_PATH/$it"))
+    private fun copySpecialSounds(soundsDirectory: SoundsDirectory) {
+        SPECIAL_SOUNDS.forEach { sound ->
+            if (soundsDirectory.getSound(sound) == null) {
+                SoundStore::class.java.classLoader.getResourceAsStream("$SPECIAL_SOUNDS_PATH/$sound")?.use { input ->
+                    FileOutputStream(File(soundsDirectory.dirName, sound)).use { output ->
+                        input.copyTo(output)
+                    }
                 }
+            } else {
+                logger.warn("Sound already exists; not copying special sound: $sound")
             }
         }
     }
@@ -201,5 +170,19 @@ class SoundStore @Autowired constructor(
             hashText = "0$hashText"
         }
         return hashText
+    }
+
+    private enum class SoundsDirectory(val dirName: String) {
+        PRIMARY("sounds"),
+        SECONDARY("sounds_alt");
+
+        fun getSound(name: String): File? {
+            val file = File(dirName, name)
+            return if (file.exists()) file else null
+        }
+
+        fun other(): SoundsDirectory {
+            return if (this == PRIMARY) SECONDARY else PRIMARY
+        }
     }
 }
