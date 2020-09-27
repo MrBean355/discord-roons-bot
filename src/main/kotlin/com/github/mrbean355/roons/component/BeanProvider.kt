f4ffeaa259d4ef89dac3ced0192a01a0a510a5b2
Mike Johnston <mrbean355@gmail.com>
2020-09-27 17:08:33 +0200

    Add Dota mod controller
    Returns a list of mods that are uploaded to the S3 bucket.

--- src/main/kotlin/com/github/mrbean355/roons/component/BeanProvider.kt
+++ src/main/kotlin/com/github/mrbean355/roons/component/BeanProvider.kt
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Scope
 import org.springframework.context.support.GenericApplicationContext
 import org.springframework.stereotype.Component
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
 
 /** Environment variable for the Discord API token. */
 private const val ENV_DISCORD_TOKEN = "DISCORD_API_TOKEN"
@@ -30,5 +32,10 @@ object BeanProvider {
         val clazz = injectionPoint.methodParameter?.containingClass
                 ?: injectionPoint.field?.declaringClass
         return LoggerFactory.getLogger(clazz)
+    }
+
+    @Bean
+    fun s3Client(): S3Client {
+        return S3Client.builder().region(Region.US_EAST_2).build()
     }
 }
