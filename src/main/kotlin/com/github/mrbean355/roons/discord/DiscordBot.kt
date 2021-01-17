4c2784b2d51d3255dc11600959147bf4508ff37c
Mike Johnston <mrbean355@gmail.com>
2021-01-17 19:18:07 +0200

    Add license to source files

--- src/main/kotlin/com/github/mrbean355/roons/discord/DiscordBot.kt
+++ src/main/kotlin/com/github/mrbean355/roons/discord/DiscordBot.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Michael Johnston
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.mrbean355.roons.discord
 
 import com.github.mrbean355.roons.DiscordBotUser
@@ -19,7 +35,10 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.OnlineStatus
-import net.dv8tion.jda.api.Permission.*
+import net.dv8tion.jda.api.Permission.MESSAGE_READ
+import net.dv8tion.jda.api.Permission.MESSAGE_WRITE
+import net.dv8tion.jda.api.Permission.VOICE_CONNECT
+import net.dv8tion.jda.api.Permission.VOICE_SPEAK
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.entities.ChannelType
 import net.dv8tion.jda.api.entities.Guild
