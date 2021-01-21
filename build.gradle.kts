86fe0e9a4053aaa1a4f70fe11b9a28acc03e8107
Mike Johnston <mrbean355@gmail.com>
2021-01-21 23:47:28 +0200

    Add some basic unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Michael Johnston
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -28,6 +44,10 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
+
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
@@ -43,4 +63,9 @@ dependencies {
     implementation("com.vdurmont:semver4j:3.1.0")
     implementation("org.telegram:telegrambots:5.0.1")
     implementation("org.telegram:telegrambots-spring-boot-starter:5.0.1")
+
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.mockk:mockk:1.10.5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.2")
 }
---
+++ src/test/kotlin/com/github/mrbean355/roons/component/PlaySoundsTest.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2021 Michael Johnston
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.mrbean355.roons.component
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import com.github.mrbean355.roons.loadTestResource
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.io.TempDir
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
+import java.io.File
+
+private const val EXPECTED_URL = "http://chatbot.admiralbulldog.live/playsounds"
+
+internal class PlaySoundsTest {
+    @MockK
+    private lateinit var soundBiteConverter: SoundBiteConverter
+
+    @MockK
+    private lateinit var listRestTemplate: RestTemplate
+
+    @MockK
+    private lateinit var downloadRestTemplate: RestTemplate
+    private lateinit var playSounds: PlaySounds
+
+    @TempDir
+    lateinit var destination: File
+
+    @BeforeEach
+    internal fun setUp() {
+        val l = LoggerFactory.getLogger("io.mockk.impl.recording.states.AnsweringState") as Logger
+        l.level = Level.OFF
+
+        MockKAnnotations.init(this)
+        every { listRestTemplate.getForEntity<String>(EXPECTED_URL) } returns mockk {
+            every { statusCode } returns HttpStatus.OK
+            every { body } returns loadTestResource("playsounds-response.html")
+        }
+        every { downloadRestTemplate.getForEntity<ByteArray>(any<String>()) } returns mockk {
+            every { statusCode } returns HttpStatus.OK
+            every { body } returns byteArrayOf()
+        }
+        playSounds = PlaySounds(soundBiteConverter, listRestTemplate, downloadRestTemplate)
+    }
+
+    @Test
+    internal fun testListRemoteFiles_CallsRestTemplate() {
+        playSounds.listRemoteFiles()
+
+        verify { listRestTemplate.getForEntity<String>(EXPECTED_URL) }
+    }
+
+    @Test
+    internal fun testListRemoteFiles_UnsuccessfulResponse_ThrowsException() {
+        every { listRestTemplate.getForEntity<String>(EXPECTED_URL) } returns mockk {
+            every { statusCode } returns HttpStatus.SERVICE_UNAVAILABLE
+            every { body } returns null
+        }
+
+        assertThrows<RuntimeException> {
+            playSounds.listRemoteFiles()
+        }
+    }
+
+    @Test
+    internal fun testListRemoteFiles_SuccessfulResponseWithNullBody_ThrowsException() {
+        every { listRestTemplate.getForEntity<String>(EXPECTED_URL) } returns mockk {
+            every { statusCode } returns HttpStatus.OK
+            every { body } returns null
+        }
+
+        assertThrows<RuntimeException> {
+            playSounds.listRemoteFiles()
+        }
+    }
+
+    @Test
+    internal fun testListRemoteFiles_SuccessfulResponseWithBody_ReturnsCorrectItems() {
+        val result = playSounds.listRemoteFiles()
+
+        assertEquals(4, result.size)
+        assertEquals("eight", result[0].name)
+        assertEquals("https://admiralbullbot.github.io/playsounds/files/bulldog/8.ogg", result[0].url)
+        assertEquals("eleven", result[1].name)
+        assertEquals("https://admiralbullbot.github.io/playsounds/files/bulldog/11.ogg", result[1].url)
+        assertEquals("ahaha4head", result[2].name)
+        assertEquals("https://admiralbullbot.github.io/playsounds/files/bulldog/ahaha4head.ogg", result[2].url)
+        assertEquals("alchemist", result[3].name)
+        assertEquals("https://admiralbullbot.github.io/playsounds/files/bulldog/alchemist.ogg", result[3].url)
+    }
+
+    @Test
+    internal fun testDownloadFile_CallsRestTemplate() {
+        justRun { soundBiteConverter.convert(any()) }
+
+        playSounds.downloadFile(PlaySounds.RemoteSoundFile("roons", "https://roons.mp3"), destination.absolutePath)
+
+        verify { downloadRestTemplate.getForEntity<ByteArray>("https://roons.mp3") }
+    }
+
+    @Test
+    internal fun testDownloadFile_UnsuccessfulResponse_ThrowsException() {
+        every { downloadRestTemplate.getForEntity<ByteArray>("https://roons.mp3") } returns mockk {
+            every { statusCode } returns HttpStatus.SERVICE_UNAVAILABLE
+            every { body } returns null
+        }
+
+        assertThrows<RuntimeException> {
+            playSounds.downloadFile(PlaySounds.RemoteSoundFile("roons", "https://roons.mp3"), destination.absolutePath)
+        }
+    }
+
+    @Test
+    internal fun testDownloadFile_SuccessfulResponseWithNullBody_ThrowsException() {
+        every { downloadRestTemplate.getForEntity<ByteArray>("https://roons.mp3") } returns mockk {
+            every { statusCode } returns HttpStatus.OK
+            every { body } returns null
+        }
+
+        assertThrows<RuntimeException> {
+            playSounds.downloadFile(PlaySounds.RemoteSoundFile("roons", "https://roons.mp3"), destination.absolutePath)
+        }
+    }
+}
