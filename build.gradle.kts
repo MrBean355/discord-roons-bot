12a71be3ef282651ab81a7c82eb7377420df571e
Mike Johnston <mrbean355@gmail.com>
2021-01-21 21:28:24 +0200

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-    id("org.springframework.boot") version "2.4.1"
+    id("org.springframework.boot") version "2.4.2"
 }
 
 group = "com.github.mrbean355"
@@ -32,12 +32,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-rest:2.4.1")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.4.1")
-    implementation("mysql:mysql-connector-java:8.0.22")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest:2.4.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.4.2")
+    implementation("mysql:mysql-connector-java:8.0.23")
     implementation("com.google.code.gson:gson:2.8.6")
 
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
     implementation("net.dv8tion:JDA:4.2.0_225")
     implementation("com.sedmelluq:lavaplayer:1.3.66")
     implementation("com.vdurmont:semver4j:3.1.0")
