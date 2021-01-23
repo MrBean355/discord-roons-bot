0c1be0959ed0bbdef8e459c18cc9de29d26cb7d4
Mike Johnston <mrbean355@gmail.com>
2021-01-23 23:10:09 +0200

    Remove /users Telegram bot command
    The statistics service should be used instead.

--- src/main/kotlin/com/github/mrbean355/roons/telegram/RoonsTelegramBot.kt
+++ src/main/kotlin/com/github/mrbean355/roons/telegram/RoonsTelegramBot.kt
@@ -16,37 +16,18 @@
 
 package com.github.mrbean355.roons.telegram
 
-import com.github.mrbean355.roons.SendHtmlMessage
-import com.github.mrbean355.roons.getTimeAgo
-import com.github.mrbean355.roons.repository.AppUserRepository
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.objects.Update
-import java.util.Calendar.MINUTE
 
 @Component
-class RoonsTelegramBot(private val appUserRepository: AppUserRepository) : TelegramLongPollingBot() {
-    private val chatId = System.getenv("TELEGRAM_CHAT").orEmpty()
+class RoonsTelegramBot : TelegramLongPollingBot() {
 
     override fun getBotUsername(): String = System.getenv("TELEGRAM_USERNAME")
 
     override fun getBotToken(): String = System.getenv("TELEGRAM_TOKEN")
 
-    override fun onUpdateReceived(update: Update) {
-        if (!update.hasMessage() || !update.message.hasText()) {
-            return
-        }
-        if (update.message.text == "/users") {
-            usersCommand()
-        }
-    }
-
-    private fun usersCommand() {
-        val activeUsers = appUserRepository.findByLastSeenAfter(getTimeAgo(5, MINUTE)).size
-        val message = buildString {
-            append("📊 <b>Active Users:</b>\n")
-            append("There have been <b>$activeUsers</b> active users in the last 5 minutes.")
-        }
-        execute(SendHtmlMessage(chatId, message))
+    override fun onUpdateReceived(update: Update?) {
+        // No-op.
     }
 }
