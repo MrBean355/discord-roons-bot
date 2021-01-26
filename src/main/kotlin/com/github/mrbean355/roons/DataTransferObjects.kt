7ab76af9871930feedbfa14eb8d1430c739758ad
Mike Johnston <mrbean355@gmail.com>
2021-01-26 17:19:05 +0200

    Remove unused DTO

--- src/main/kotlin/com/github/mrbean355/roons/DataTransferObjects.kt
+++ src/main/kotlin/com/github/mrbean355/roons/DataTransferObjects.kt
@@ -21,41 +21,35 @@ data class CreateIdResponse(val userId: String)
 data class AnalyticsRequest(val userId: String, val properties: Map<String, String>)
 
 data class PlaySoundRequest(
-        val userId: String,
-        val token: String,
-        val soundFileName: String,
-        /** Only present from app 1.10.0 */
-        val volume: Int?,
-        /** Only present from app 1.10.0 */
-        val rate: Int?
+    val userId: String,
+    val token: String,
+    val soundFileName: String,
+    /** Only present from app 1.10.0 */
+    val volume: Int?,
+    /** Only present from app 1.10.0 */
+    val rate: Int?
 )
 
 data class PlaySoundsRequest(
-        val userId: String,
-        val token: String,
-        val sounds: List<SingleSound>
+    val userId: String,
+    val token: String,
+    val sounds: List<SingleSound>
 )
 
 data class SingleSound(
-        val soundFileName: String,
-        val volume: Int,
-        val rate: Int
-)
-
-data class StatisticsResponse(
-        val recentUsers: Int,
-        val dailyUsers: Int,
-        val properties: Map<String, Map<String, Int>>
+    val soundFileName: String,
+    val volume: Int,
+    val rate: Int
 )
 
 data class DotaModDto(
-        val key: String,
-        val name: String,
-        val description: String,
-        val size: Int,
-        val hash: String,
-        val downloadUrl: String,
-        val infoUrl: String
+    val key: String,
+    val name: String,
+    val description: String,
+    val size: Int,
+    val hash: String,
+    val downloadUrl: String,
+    val infoUrl: String
 )
 
 fun DotaMod.asDto(): DotaModDto = DotaModDto(key, name, description, size, hash, downloadUrl, infoUrl)
