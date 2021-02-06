cb1fe4a7296e9778cdfddd180904b45754f17fff
Mike Johnston <mrbean355@gmail.com>
2021-02-06 17:34:41 +0200

    Replace RequestMapping with PostMapping for readability

--- src/main/kotlin/com/github/mrbean355/roons/controller/AnalyticsController.kt
+++ src/main/kotlin/com/github/mrbean355/roons/controller/AnalyticsController.kt
@@ -20,9 +20,9 @@ import com.github.mrbean355.roons.AnalyticsRequest
 import com.github.mrbean355.roons.component.Analytics
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -31,7 +31,7 @@ class AnalyticsController @Autowired constructor(
     private val analytics: Analytics
 ) {
 
-    @RequestMapping("logProperties", method = [POST])
+    @PostMapping("logProperties")
     fun logProperties(@RequestBody request: AnalyticsRequest): ResponseEntity<Void> {
         return if (analytics.logProperties(request.userId, request.properties)) {
             ResponseEntity.ok()
