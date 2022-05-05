d633d3cb5c71fe64dbbead78097dbabbdf96b07d
Mike Johnston <mrbean355@gmail.com>
2022-05-05 21:00:41 +0200

    Create separate command to set the volume

--- src/main/kotlin/com/github/mrbean355/roons/discord/commands/SetVolumeCommand.kt
+++ src/main/kotlin/com/github/mrbean355/roons/discord/commands/SetVolumeCommand.kt
@@ -16,23 +16,33 @@
 
 package com.github.mrbean355.roons.discord.commands
 
+import com.github.mrbean355.roons.discord.audio.coerceVolume
 import com.github.mrbean355.roons.repository.DiscordBotSettingsRepository
 import com.github.mrbean355.roons.repository.loadSettings
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 import org.springframework.stereotype.Component
 
+private const val NewVolumeOption = "new-volume"
+
 @Component
-class VolumeCommand(
+class SetVolumeCommand(
     private val discordBotSettingsRepository: DiscordBotSettingsRepository
 ) : BotCommand {
 
-    override val name get() = "volume"
-    override val description get() = "Check the volume of sounds played in voice channels."
+    override val name get() = "set-volume"
+    override val description get() = "Set the volume of sounds played in voice channels."
+
+    override fun buildCommand(commandData: SlashCommandData) = commandData
+        .addOption(OptionType.INTEGER, NewVolumeOption, "Volume percentage", true)
 
     override fun handleCommand(event: SlashCommandInteractionEvent) {
         val member = event.member ?: return
         val settings = discordBotSettingsRepository.loadSettings(member.guild.id)
+        val newVolume = event.getOption(NewVolumeOption)?.asInt?.coerceVolume() ?: return
 
-        event.reply("My volume is at `${settings.volume}%`.").setEphemeral(true).queue()
+        discordBotSettingsRepository.save(settings.copy(volume = newVolume))
+        event.reply("My volume has been set to `${newVolume}%`.").queue()
     }
 }
