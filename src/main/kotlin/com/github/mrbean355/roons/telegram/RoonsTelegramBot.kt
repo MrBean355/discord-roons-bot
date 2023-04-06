24d6223a2b5ca0cd68fdfc67a9756b6ceb913007
Mike Johnston <mrbean355@gmail.com>
2023-04-06 23:09:10 +0200

    Replace usage of deprecated constructor

--- src/main/kotlin/com/github/mrbean355/roons/telegram/RoonsTelegramBot.kt
+++ src/main/kotlin/com/github/mrbean355/roons/telegram/RoonsTelegramBot.kt
@@ -21,11 +21,11 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.objects.Update
 
 @Component
-class RoonsTelegramBot : TelegramLongPollingBot() {
+class RoonsTelegramBot : TelegramLongPollingBot(
+    System.getenv("TELEGRAM_TOKEN")
+) {
 
     override fun getBotUsername(): String = System.getenv("TELEGRAM_USERNAME")
-
-    override fun getBotToken(): String = System.getenv("TELEGRAM_TOKEN")
 
     override fun onUpdateReceived(update: Update?) {
         // No-op.
