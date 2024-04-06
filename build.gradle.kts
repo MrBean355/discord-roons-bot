e8b897716d59e7d4e6e6dbe8796b76e261441d47
Mike Johnston <mrbean355@gmail.com>
2024-04-06 10:08:49 +0200

    Heroku test

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-rest:3.2.4")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
-    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("org.postgresql:postgresql:42.7.3")
     implementation("com.google.code.gson:gson:2.10.1")
 
     implementation("net.dv8tion:JDA:5.0.0-beta.21")
